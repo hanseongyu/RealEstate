e3435167e160811069bcaa7ae0c741a0a150b127
richer <persona1241@naver.com>
2023-06-13 07:02:11 +0900

    Initial commit

---
+++ app/src/main/java/com/example/realestate/adapters/RoomAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.realestate.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.realestate.R
+import com.example.realestate.datas.Room
+
+class RoomAdapter(
+    val mContext : Context,
+    val resId:Int,
+    val mList: ArrayList<Room>) : ArrayAdapter<Room>(mContext,resId,mList) {
+
+        val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null){
+            tempRow = inflater.inflate(R.layout.room_list_item,null)
+        }
+        val row = tempRow!!
+
+        return row
+    }
+}
---
+++ app/src/main/java/com/example/realestate/datas/Room.kt
@@ -0,0 +1,9 @@
+package com.example.realestate.datas
+
+class Room(
+    val price: Int,
+    val address: String,
+    val floor: Int,
+    val descrription: String) {
+
+}
